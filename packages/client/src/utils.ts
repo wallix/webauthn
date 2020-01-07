640c697e6900768a933d161454f28dd14f75cf3f
Matthew Miller <matthew@millerti.me>
2020-01-07 10:06:07 -0800

    Add typing to publicKeyCredentialToJSON()

--- packages/client/src/utils.ts
+++ packages/client/src/utils.ts
@@ -1,18 +1,20 @@
-export const publicKeyCredentialToJSON = (
-    item
-) => {
+import { Unibabel } from 'unibabel';
+
+type Credential = ArrayBuffer[] | ArrayBuffer | { [key: string]: Credential };
+type CredentialJSON = string | Object | (string | Object)[];
+
+export const publicKeyCredentialToJSON = (item: Credential): CredentialJSON => {
     if (item instanceof Array) {
         return item.map(publicKeyCredentialToJSON);
     }
 
     if (item instanceof ArrayBuffer) {
-        const { Unibabel } = require('unibabel');
         // ArrayBuffer must be converted to typed arrays
         return Unibabel.bufferToBase64(new Uint8Array(item));
     }
 
     if (item instanceof Object) {
-        const obj = {};
+        const obj: { [key: string]: CredentialJSON } = {};
 
         // tslint:disable-next-line
         for (const key in item) {
