d9387e998b53685022af075007284988c1514358
Matthew Miller <matthew@millerti.me>
2020-01-07 12:27:22 -0800

    Update solveLoginChallenge with stronger typing

--- packages/client/src/solveLoginChallenge.ts
+++ packages/client/src/solveLoginChallenge.ts
@@ -1,25 +1,46 @@
-import  { publicKeyCredentialToJSON } from './utils';
+import  { publicKeyCredentialToJSON, AssertionCredential, stringToBuffer } from './utils';
 
-const loginChallengeToPublicKey = getAssert => {
-    const { Unibabel } = require('unibabel');
+interface AllowedCredentialJSON extends Omit<PublicKeyCredentialDescriptor, 'id'> {
+    id: string;
+}
+
+/**
+ * JSON-ified options to be passed into navigator.credentials.get(). These values are requested
+ * from the Relying Party (see `server > generateLoginChallenge()`), which responds with JSON
+ * containing ArrayBuffers converted to base64-encoded strings.
+ */
+interface LoginChallengeJSON extends Omit<PublicKeyCredentialRequestOptions, 'challenge' | 'allowCredentials'> {
+    challenge: string; // A base64-encoded Buffer
+    allowCredentials: AllowedCredentialJSON[];
+}
+
+/**
+ * Convert JSON-ified credential options into values for use in navigator.credentials.get()
+ */
+function loginChallengeToPublicKey (challenge: LoginChallengeJSON): PublicKeyCredentialRequestOptions {
+    let allowCredentials: PublicKeyCredentialDescriptor[];
+    if (Array.isArray(challenge.allowCredentials) && challenge.allowCredentials.length > 0) {
+        allowCredentials = challenge.allowCredentials.map(allowCredential => ({
+            ...allowCredential,
+            id: stringToBuffer(allowCredential.id),
+        }));
+    }
 
     return {
-        ...getAssert,
-        challenge: Unibabel.base64ToBuffer(getAssert.challenge),
-        allowCredentials: getAssert.allowCredentials.map(allowCredential => ({
-            ...allowCredential,
-            id: Unibabel.base64ToBuffer(allowCredential.id),
-        })),
+        ...challenge,
+        challenge: stringToBuffer(challenge.challenge),
+        // @ts-ignore 2454
+        allowCredentials,
     };
 };
 
-export const solveLoginChallenge = async credentialsChallengeRequest => {
-    const publicKey = loginChallengeToPublicKey(credentialsChallengeRequest);
-
-    // @ts-ignore
-    const credentials = await navigator.credentials.get({
-        publicKey,
-    });
+/**
+ * Initiate the Webauthn Assertion process, then convert the results to JSON to POST back to the
+ * Relying Party (see `server > parseLoginRequest()`)
+ */
+export const solveLoginChallenge = async (challenge: LoginChallengeJSON) => {
+    const publicKey = loginChallengeToPublicKey(challenge);
+    const credentials = (await navigator.credentials.get({ publicKey }) as AssertionCredential);
 
     return publicKeyCredentialToJSON(credentials);
 };
