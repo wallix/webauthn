22e8464f14f7d644cd9acb85d473f8c8315bc2c6
Matthew Miller <matthew@millerti.me>
2020-01-07 11:41:06 -0800

    Add stronger typing to publicKeyCredentialToJSON()

--- packages/client/src/utils.ts
+++ packages/client/src/utils.ts
@@ -1,28 +1,85 @@
 import { Unibabel } from 'unibabel';
 
-type Credential = ArrayBuffer[] | ArrayBuffer | { [key: string]: Credential };
-type CredentialJSON = string | Object | (string | Object)[];
-
-export const publicKeyCredentialToJSON = (item: Credential): CredentialJSON => {
-    if (item instanceof Array) {
-        return item.map(publicKeyCredentialToJSON);
+/**
+ * Values returned from navigator.credentials.create()
+ */
+export interface AttestationCredential extends PublicKeyCredential {
+    rawId: ArrayBuffer;
+    response: {
+        attestationObject: ArrayBuffer;
+        clientDataJSON: ArrayBuffer;
     }
+}
 
-    if (item instanceof ArrayBuffer) {
-        // ArrayBuffer must be converted to typed arrays
-        return Unibabel.bufferToBase64(new Uint8Array(item));
+/**
+ * AttestationCredentials with ArrayBuffers converted to base64-encoded strings for JSON
+ * compatibility
+ */
+export interface AttestationCredentialJSON extends Omit<AttestationCredential, 'rawId' | 'response'> {
+    rawId: string;
+    response: {
+        attestationObject: string;
+        clientDataJSON: string;
     }
+}
 
-    if (item instanceof Object) {
-        const obj: { [key: string]: CredentialJSON } = {};
-
-        // tslint:disable-next-line
-        for (const key in item) {
-            obj[key] = publicKeyCredentialToJSON(item[key]);
-        }
-
-        return obj;
+/**
+ * Values returned from navigator.credentials.get()
+ */
+export interface AssertionCredential extends PublicKeyCredential {
+    rawId: ArrayBuffer;
+    response: {
+        authenticatorData: ArrayBuffer;
+        clientDataJSON: ArrayBuffer;
+        signature: ArrayBuffer;
+        userHandle: ArrayBuffer;
     }
+}
 
-    return item;
+/**
+ * AssertionCredentials with ArrayBuffers converted to base64-encoded strings for JSON
+ * compatibility
+ */
+export interface AssertionCredentialJSON extends Omit<AssertionCredential, 'rawId' | 'response'> {
+    rawId: string;
+    response: {
+        authenticatorData: string;
+        clientDataJSON: string;
+        signature: string;
+        userHandle: string;
+    }
+}
+
+type WebauthnCredential = AttestationCredential | AssertionCredential;
+type WebauthnCredentialJSON = AttestationCredentialJSON | AssertionCredentialJSON;
+
+/**
+ * Convenience method for converting an ArrayBuffer to a base64-encoded string
+ */
+function bufferToString(buffer: ArrayBuffer): string {
+    return Unibabel.bufferToBase64(new Uint8Array(buffer));
+}
+
+export function publicKeyCredentialToJSON (credential: WebauthnCredential): WebauthnCredentialJSON {
+    if ('attestationObject' in credential.response) {
+        return {
+            ...credential,
+            rawId: bufferToString(credential.rawId),
+            response: {
+                attestationObject: bufferToString(credential.response.attestationObject),
+                clientDataJSON: bufferToString(credential.response.clientDataJSON),
+            },
+        } as AttestationCredentialJSON;
+    } else {
+        return {
+            ...credential,
+            rawId: bufferToString(credential.rawId),
+            response: {
+                authenticatorData: bufferToString(credential.response.authenticatorData),
+                clientDataJSON: bufferToString(credential.response.clientDataJSON),
+                signature: bufferToString(credential.response.signature),
+                userHandle: bufferToString(credential.response.userHandle),
+            }
+        } as AssertionCredentialJSON;
+    }
 };
